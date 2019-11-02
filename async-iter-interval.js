590f5a370249be6ff1294329addc6734c80642f8
jauntywunderkind <jaunty+wunder+kind+git@voodoowarez.com>
2019-11-02 17:29:38 -0400

    add 'end' which calls 'clearInterval' & super

--- async-iter-interval.js
+++ async-iter-interval.js
@@ -12,17 +12,24 @@ export class AsyncIterInterval extends AsyncIterPipe{
 		}
 	}
 	setInterval(){
+		if( this.done){
+			throw new Error( "already done")
+		}
 		if( this.interval){ // hi ho hello implicit state machine
 			return
 		}
 		this.interval= setInterval( this.tick, this.ms)
 		return this
 	}
+	end(){
+		this.clearInterval()
+		return super.end()
+	}
 	clearInterval(){
 		if( this.interval){
 			clearInterval( this.interval)
+			this.interval= null
 		}
-		this.interval= null
 		return this
 	}
 	async return( value){
